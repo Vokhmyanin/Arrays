b887dc1771239a80f73aef066bded930b8d07aac
Евгений <@LAPTOP-IEGUVAC3>
2023-05-31 17:12:21 +0700

    done

--- Sorting/Source.cpp
+++ Sorting/Source.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 #define tab "\t"
 //#define SORTING
-#define RANGE_0_5
+
 //#define RANGE_70_80
 void main()
 {
@@ -54,42 +54,7 @@ void main()
 	}
 	cout << endl;
 #endif
-#ifdef RANGE_0_5
-	const int n = 10;
-	int arr[n];
-	for (int i = 0; i < n; i++)
-	{
-		arr[i] = rand() % 5;
-	}
-	for (int i = 0; i < n; i++)
-	{
-		cout << arr[i] << "\t";
-	}
-	cout << endl;
-	for (int i = 0; i < n; i++)
-	{
-		bool met_before = false; //
-		for (int j = 0; j < i; j++)
-		{
-			if (arr[i] == arr[j])
-			{
-				met_before = true; //запоминаем что число встречалось ранее
-				break; //прерывает текущую итерацию и все последующие
-			}
-		}
-		if (met_before)continue;// прерывает текущую итерацию и переходит к следующей
-		// поиск повторений
-		int count = 0;
-		for (int j = i + 1; j < n; j++)
-		{
-			if (arr[i] == arr[j])
-			{
-				count++;
-			}
-		}
-		if (count)printf("Значение %d повторяется %d раз\n", arr[i], count);
-	}
-#endif
+
 #ifdef RANGE_70_80
 	const int n = 10;
 	int arr[n];
